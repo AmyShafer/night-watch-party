213f34543d75d6163d2a07716fcd31ce31232f91
Amy Shafer <amy.u.shafer@gmail.com>
2025-05-27 00:54:32 -0400

    auth js file

---
+++ server/utils/auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+const secret = '';
+const expiration = '2hr';
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+      let token = req.body.token || req.query.token || req.headers.authorization;
+
+      if (req.headers.authorization) {
+        token = token
+          .split(' ')
+          .pop()
+          .trim();
+      }
+      console.log('token', token)
+
+      if (!token) {
+        return req;
+      }
+
+      try {
+        const { data } = jwt.verify(token, secret, {maxAge: expiration });
+      }
+      catch {
+        console.log('Invalid token');
+      }
+
+      return req;
+    },
+    signToken: function ({ userName, email, _id }) {
+      const payload = { userName, email, _id };
+
+      return jwt.sign(
+        { data: payload },
+        secret,
+        { expiresIn: expiration }
+    );
+  }
+};
